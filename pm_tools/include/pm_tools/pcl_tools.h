2e5d1ac87cf0d81b41afaf5cc187c468e8b739cf
David Fornas <davidfornas@gmail.com>
2016-07-07 10:05:24 +0200

    Add Eigen snippets to pcl_tools

--- pm_tools/include/pm_tools/pcl_tools.h
+++ pm_tools/include/pm_tools/pcl_tools.h
@@ -178,4 +178,44 @@ public:
 	}
 };
 
+class EigenTools
+{
+
+static Eigen::Quaterniond
+euler2Quaternion( const double roll,
+                  const double pitch,
+                  const double yaw )
+{
+    Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
+    Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
+
+    Eigen::Quaterniond q = yawAngle * pitchAngle * rollAngle;
+
+    //Eigen::Quaternion<double> q = rollAngle * yawAngle * pitchAngle;
+    //Eigen::Matrix3d rotationMatrix = q.matrix();
+
+    return q;
+}
+
+static Eigen::Quaterniond
+euler2Matrix( const double roll,
+                  const double pitch,
+                  const double yaw )
+{
+    Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
+    Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
+
+    Eigen::Quaternion<double> q = rollAngle * yawAngle * pitchAngle;
+    Eigen::Matrix3d rotationMatrix = q.matrix();
+
+    return q;
+}
+
+};
+
+
+
+
 #endif
