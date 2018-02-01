70455b4d4dba176cef830415c93c4562393ff4b6
David Fornas <davidfornas@gmail.com>
2018-02-01 10:35:56 +0100

    Refactor to generalize PoseEstimation from SACPoseEstimation

--- pm_perception/include/pm_perception/sac_pose_estimation.h
+++ pm_perception/include/pm_perception/sac_pose_estimation.h
@@ -26,49 +26,36 @@
 typedef pcl::PointXYZRGB PointT;
 typedef pcl::PointCloud<PointT>::Ptr PointTPtr;
 
-class SACPoseEstimation{
+class PoseEstimation{
+
+protected:
 
   PointTPtr cloud_;
   std::string camera_frame_name, topic_name;
-
-  //Punto central del cilindro y la direccion.
-  PointT axis_point_g;
-  tf::Vector3 normal_g;
   FrameToTF vispToTF;
 
-  pcl::ModelCoefficients::Ptr coefficients_plane, coefficients_cylinder;
-  double plane_distance_threshold_, cylinder_distance_threshold_, radious_limit_;
-  int plane_iterations_, cylinder_iterations_;
+  pcl::ModelCoefficients::Ptr coefficients_plane;
+  double plane_distance_threshold_;
+  int plane_iterations_;
   bool ransac_background_filter_;
 
 public:
 
   vpHomogeneousMatrix  cMo; ///< object frame with respect to the camera after detection
-  double radious, height;
 
-  /** Constructor.
-   * @params: Get pose using RANSAC & the input cloud.
-   * */
-  SACPoseEstimation(PointTPtr cloud, double distanceThreshold = 0.05){//, ros::NodeHandle & nh, std::string object_pose, vpHomogeneousMatrix wMc ){
+  PoseEstimation(PointTPtr cloud, double distanceThreshold = 0.05){
     setPlaneSegmentationParams(distanceThreshold);
-    setCylinderSegmentationParams();
-
     cloud_ = cloud;
+    ransac_background_filter_ = true;
     camera_frame_name = cloud->header.frame_id;
     coefficients_plane = (pcl::ModelCoefficients::Ptr) new pcl::ModelCoefficients;
-    ransac_background_filter_ = true;
-  }
 
-  /** Set background removal mode */
-  void setRansacBackgroundFilter( bool ransac_background_filter ){ ransac_background_filter_ = ransac_background_filter; }
+  }
 
   void setNewCloud(PointTPtr cloud){cloud_ = cloud;}
 
-  /** Where segmentation is done */
-  void doRansac();
-
-  /** Online planning **/
-  void redoRansac();
+  /** Set background removal mode */
+  void setRansacBackgroundFilter( bool ransac_background_filter ){ ransac_background_filter_ = ransac_background_filter; }
 
   /** Set plane segmentation parameters: distance to the inliers to the plane
    * and number of iterations.
@@ -78,6 +65,44 @@ public:
     plane_iterations_=iterations;
   }
 
+  void setCamerFrameName( std::string name){
+    camera_frame_name = name;
+  }
+
+  /** Get the object frame with respect to the camera frame */
+  vpHomogeneousMatrix get_cMo() {return cMo;}
+};
+
+
+
+class SACPoseEstimation : public PoseEstimation{
+
+  //Punto central del cilindro y la direccion.
+  PointT axis_point_g;
+  tf::Vector3 normal_g;
+
+  pcl::ModelCoefficients::Ptr coefficients_cylinder;
+  double cylinder_distance_threshold_, radious_limit_;
+  int cylinder_iterations_;
+
+public:
+
+  double radious, height;
+
+  /** Constructor.
+   * @params: Get pose using RANSAC & the input cloud.
+   * */
+  SACPoseEstimation(PointTPtr cloud, double distanceThreshold = 0.05) : PoseEstimation(cloud, distanceThreshold){//, ros::NodeHandle & nh, std::string object_pose, vpHomogeneousMatrix wMc ){
+    setCylinderSegmentationParams();
+
+  }
+
+  /** Where segmentation is done */
+  void doRansac();
+
+  /** Online planning **/
+  void redoRansac();
+
   /** Set cylinder segmentation parameters: distance to the inliers to the plane,
    * number of iterations and radious limit.
    */
@@ -86,13 +111,6 @@ public:
     cylinder_iterations_=iterations;
     radious_limit_=rlimit;
   }
-
-  void setCamerFrameName( std::string name){
-    camera_frame_name = name;
-  }
-
-  /** Get the object frame with respect to the camera frame */
-  vpHomogeneousMatrix get_cMo() {return cMo;}
 
   ~SACPoseEstimation() {}
 
