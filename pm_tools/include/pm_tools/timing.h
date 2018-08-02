a3cab1790032ae49ec9f162a2fef8ae39afaba0e
David Fornas <davidfornas@gmail.com>
2018-08-02 20:40:47 +0200

    improve temporization

--- pm_tools/include/pm_tools/timing.h
+++ pm_tools/include/pm_tools/timing.h
@@ -9,32 +9,27 @@
 //Time measures with C library, should use another library such as ROS
 #include <ctime>
 #include <ros/ros.h>
+#include <std_msgs/Float32.h>
 
 class Timing
 {
-	clock_t start_time_, last_lap_time_;
+	clock_t startTime_, lastLapTime_;
+
 public:
 
-  Timing()
-  {
-	  start_time_ = clock();
-	  last_lap_time_ = clock();
-  }
+  Timing();
 
-  /** Display current time and total time. **/
-  bool lap(){
-	  clock_t end = clock();
-	  ROS_DEBUG_STREAM("Elapsed time: " << double(end - last_lap_time_) / CLOCKS_PER_SEC << "Total time: " << double(end - start_time_) / CLOCKS_PER_SEC);
-	  last_lap_time_ = clock();
-  }
+	void resetTimer();
 
-  /** Display total time and restart. **/
-  bool total(){
-	  clock_t end = clock();
-	  ROS_DEBUG_STREAM("Total time: " << double(end - start_time_) / CLOCKS_PER_SEC);
-	  last_lap_time_ = clock();
-	  start_time_ = clock();
-  }
+	double getLapTimeWithDebug();
+	double getLapTime();
+	std_msgs::Float32 getLapTimeMsg();
+
+	double getTotalTimeWithDebug();
+	double getTotalTime();
+	std_msgs::Float32 getTotalTimeMsg();
+
+	static std_msgs::Float32 toFloat32Msgs(const float msg);
 
 };
 
--- pm_tools/src/timing.cpp
+++ pm_tools/src/timing.cpp
@@ -6,4 +6,62 @@
  */
 #include <pm_tools/timing.h>
 
+Timing::Timing()
+{
+  startTime_ = clock();
+  lastLapTime_ = clock();
+}
 
+double Timing::getLapTimeWithDebug(){
+  clock_t end = clock();
+  double elapsed =  double(end - lastLapTime_) / CLOCKS_PER_SEC;
+  ROS_DEBUG_STREAM("Elapsed time: " << elapsed << "Total time: " << double(end - lastLapTime_) / CLOCKS_PER_SEC);
+  lastLapTime_ = clock();
+  return elapsed;
+}
+
+double Timing::getLapTime(){
+  clock_t end = clock();
+  double elapsed =  double(end - lastLapTime_) / CLOCKS_PER_SEC;
+  lastLapTime_ = clock();
+  return elapsed;
+}
+
+std_msgs::Float32 Timing::getLapTimeMsg(){
+  clock_t end = clock();
+  double elapsed =  double(end - lastLapTime_) / CLOCKS_PER_SEC;
+  lastLapTime_ = clock();
+  return toFloat32Msgs(elapsed);
+}
+
+double Timing::getTotalTimeWithDebug(){
+  clock_t end = clock();
+  double elapsed = double(end - startTime_) / CLOCKS_PER_SEC;
+  ROS_DEBUG_STREAM("Total time: " << elapsed);
+  return elapsed;
+}
+
+double Timing::getTotalTime(){
+  clock_t end = clock();
+  double elapsed = double(end - startTime_) / CLOCKS_PER_SEC;
+  ROS_DEBUG_STREAM("Total time: " << elapsed);
+  return elapsed;
+}
+
+std_msgs::Float32 Timing::getTotalTimeMsg(){
+  clock_t end = clock();
+  double elapsed = double(end - startTime_) / CLOCKS_PER_SEC;
+  ROS_DEBUG_STREAM("Total time: " << elapsed);
+  return toFloat32Msgs(elapsed);
+}
+
+std_msgs::Float32 Timing::toFloat32Msgs(float msg){
+  std_msgs::Float32 rosMsg;
+  rosMsg.data = msg;
+  return rosMsg;
+}
+
+void Timing::resetTimer(){
+  startTime_ = clock();
+  lastLapTime_ = clock();
+}
