f04fe64ecc1a14c7e8bb14520ee9fb84a0fc2abf
David Fornas <davidfornas@gmail.com>
2018-08-21 21:17:14 +0200

    Add independent sphere test

---
+++ pm_apps/src/sphere_pose_estimation_test.cpp
@@ -0,0 +1,58 @@
+/** 
+ * Test PoseEstimation and variants
+ *
+ *  Created on: 01/02/2018
+ *      Author: dfornas
+ */
+
+#include <pm_tools/pcl_tools.h>
+#include <pm_perception/ransac_pose_estimation.h>
+
+typedef pcl::PointXYZRGB PointT;
+typedef typename pcl::PointCloud<PointT> Cloud;
+typedef typename pcl::PointCloud<PointT>::Ptr CloudPtr;
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "pose_estimation_test");
+  ros::NodeHandle nh;
+
+  pcl::PointCloud<PointT>::Ptr point_cloud_ptr(new pcl::PointCloud<PointT>), aux_cloud(new pcl::PointCloud<PointT>),
+                               original_cloud(new pcl::PointCloud<PointT>), object_cloud(new pcl::PointCloud<PointT>);
+  //std::string filename("/stereo_camera/points2");
+  std::string filename("/stereo/points2");
+
+  SpherePoseEstimation * pose_est;
+  pose_est = new SpherePoseEstimation(nh, point_cloud_ptr);
+
+  pose_est->setDebug(true);
+  pose_est->setPlaneSegmentationParams(0.05);
+  pose_est->setSphereSegmentationParams(0.25, 20000, 0.3);
+
+  while(ros::ok()){
+    PCLTools<PointT>::cloudFromTopic(point_cloud_ptr, filename);
+    pcl::copyPointCloud(*point_cloud_ptr, *original_cloud);
+    PCLTools<PointT>::removeNanPoints(point_cloud_ptr);
+    PCLTools<PointT>::applyZAxisPassthrough(point_cloud_ptr,0, 3.5);
+    PCLTools<PointT>::applyVoxelGridFilter(point_cloud_ptr, 0.01);
+
+    pose_est->setNewCloud(point_cloud_ptr);
+    bool success = pose_est->process();
+    object_cloud = pose_est->getObjectCloud();
+
+    pcl::visualization::PCLVisualizer *p;
+    p = new pcl::visualization::PCLVisualizer("Cylinde + SQ colored");
+    //pcl::visualization::PointCloudColorHandlerCustom<PointT> tgt_h(point_cloud_ptr, 0, 230, 0);
+    pcl::visualization::PointCloudColorHandlerCustom<PointT> src_h(aux_cloud, 0, 0, 230);
+    pcl::visualization::PointCloudColorHandlerCustom<PointT> src_h2(object_cloud, 250, 0, 0);
+    p->setBackgroundColor(0.8, 0.8, 0.8);
+    p->addPointCloud(original_cloud, "c1");
+    p->addPointCloud(aux_cloud, src_h, "c2");
+    p->addPointCloud(object_cloud, src_h2, "c3");
+    p->spin();
+
+  }
+  return 0;
+}
+
